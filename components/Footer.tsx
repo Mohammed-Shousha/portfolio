5c57b60821652cb57c5ebb39174d3ca8af33c9d5
Mohammed Shousha <mshousha29@gmail.com>
2023-04-10 06:16:48 +0200

    footer

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => (
+  <footer className="absolute bottom-0 left-0 pb-3 w-screen">
+    <div className=" flex justify-center">
+      <h3 className="text-xs md:text-sm lg:text-base font-thin">
+        Mohammed Shousha &copy; 2023
+      </h3>
+    </div>
+  </footer>
+);
+
+export default Footer;
